57df382b13dcf9f00be0bd3baa8a59014bc4cfd4
kir7741 <typhoonjoe2003@yahoo.com.tw>
2019-07-29 00:05:38 +0800

    完成部分 template 樣式

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ body {
   background-color: $bgGray;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: 100%;
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,18 +4,75 @@
       <div class="cal-header">
 
       </div>
-      <div class="cal-body"></div>
+      <div class="cal-body">
+        <div class="number-pad">
+
+          <!-- 數字按鈕區域開始 -->
+          <div>
+            <div
+              class="lattice"
+              v-for="(char, index) in numericCharacters"
+              :key="index"
+            >
+              <span >
+                {{ char }}
+              </span>
+            </div>
+          </div>
+          <!-- 數字按鈕區域結束 -->
+
+          <!-- 操作符區域開始 -->
+          <div>
+            <div class="lattice">
+              <span>÷</span>
+            </div>
+            <div class="lattice">
+              <span>×</span>
+            </div>
+            <div class="lattice">
+              <span>+</span>
+            </div>
+            <div class="lattice">
+              <span>−</span>
+            </div>
+          </div>
+          <!-- 操作符區域結束 -->
+
+        </div>
+
+        <!-- 特殊功能按鈕開始 -->
+        <div class="feature-pad">
+          <div class="lattice">
+            <span>AC</span>
+          </div>
+          <div class="lattice">
+            <span>⌫</span>
+          </div>
+          <div class="lattice large">
+            <div class="across">
+              <span></span>
+              <span>=</span>
+            </div>
+          </div>
+        </div>
+        <!-- 特殊功能按鈕結束 -->
+
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Calculator'
+  name: 'Calculator',
+  data() {
+    return {
+      numericCharacters: ['.', '00', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    }
+  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import '../scss/_mixin.scss';
 .wrapper {
@@ -35,9 +92,55 @@ export default {
   background-color: $darkBlue;
 }
 .cal-body {
+  display: flex;
+  flex-direction: column;
   height: 415px;
   background-color: $primaryBlue;
   border-bottom-left-radius: 10px;
   border-bottom-right-radius: 10px;
+  .number-pad {
+    display: flex;
+    flex-direction: row;
+    div:first-child {
+      display: flex;
+      flex-wrap: wrap;
+      flex: 3;
+      .lattice {
+        flex: 0 0 33.33333%;
+      }
+    }
+    div:last-child {
+      flex: 1;
+    }
+  }
+}
+.feature-pad {
+  display: flex;
+  .lattice {
+    flex: 1;
+    &.large {
+      flex: 2;
+    }
+  }
+}
+.across {
+  display: flex;
+  width: 100%;
+}
+.lattice {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80px;
+  color: #ffffff;
+  span {
+    display: inline-flex;  
+    align-items: center;
+    justify-content: center;  
+    padding: 8px;  
+    width: 100%;
+    height: 100%;
+    font-size: 24px;
+  }
 }
 </style>
